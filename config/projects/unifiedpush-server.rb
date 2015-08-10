48c067dd00c983977ff74ea4dbfdb6b706711196
yanivn <yanivn@c-b4.local>
2015-08-10 16:17:20 +0300

    Initial Commit

---
+++ config/projects/unifiedpush-server.rb
@@ -0,0 +1,29 @@
+#
+# Copyright 2015 YOUR NAME
+#
+# All Rights Reserved.
+#
+
+name "unifiedpush-server"
+maintainer "Yaniv Marom-Nachumi"
+homepage "https://c-b4.com"
+
+# Defaults to C:/unifiedpush-server on Windows
+# and /opt/unifiedpush-server on all other platforms
+install_dir "#{default_root}/#{name}"
+
+build_version Omnibus::BuildVersion.semver
+build_iteration 1
+
+# Creates required build directories
+dependency "preparation"
+
+# unifiedpush-server dependencies/components
+dependency "postgresql"
+
+# Version manifest file
+dependency "version-manifest"
+
+exclude "**/.git"
+exclude "**/bundler/git"
+
