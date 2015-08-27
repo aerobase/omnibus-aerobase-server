85e3cb4870af7669a89b1cc1025987cb8beb66bb
yanivn <yanivn@c-b4.local>
2015-08-27 11:28:37 +0300

    Add postgresql jdbc driver do wildfly modules

--- files/unifiedpush-cookbooks/unifiedpush/recipes/unifiedpush-server-wildfly-conf.rb
+++ files/unifiedpush-cookbooks/unifiedpush/recipes/unifiedpush-server-wildfly-conf.rb
@@ -15,20 +15,16 @@
 # limitations under the License.
 #
 
-require 'openssl'
-
 # Default location of install-dir is /opt/unifiedpush/. This path is set during build time.
 # DO NOT change this value unless you are building your own Unifiedpush packages
-install_dir = node['package']['install-dir']
-ENV['PATH'] = "#{install_dir}/bin:#{install_dir}/embedded/bin:#{ENV['PATH']}"
 
+install_dir = node['package']['install-dir']
 server_dir = node['unifiedpush']['unifiedpush-server']['dir']
-server_log_dir = node['unifiedpush']['unifiedpush-server']['log_directory']
+modules_dir = "#{server_dir}/modules/org/postgresql/main"
 
 # These directories do not need to be writable for unifiedpush-server
-[ 
-  server_dir,
-  server_log_dir
+[
+  modules_dir 
 ].each do |dir_name|
   directory dir_name do
     owner 'root'
@@ -38,26 +34,27 @@ server_log_dir = node['unifiedpush']['unifiedpush-server']['log_directory']
   end
 end
 
-execute 'extract_wildfly' do
-  command "tar xzvf #{install_dir}/embedded/apps/wildfly/wildfly-8.2.1.Final.tar.gz --strip-components 1"
-  cwd "#{server_dir}"
-
-  not_if { File.exists?(server_dir + "/README.txt") }
+# Update configuration 
+template "#{server_dir}/bin/standalone.conf" do
+  owner "root"
+  group node['unifiedpush']['user']['group']
+  mode 0755
+  source "wildfly-standalone.conf.erb"
 end
 
-include_recipe "unifiedpush::unifiedpush-server-wildfly-conf"
-
-runit_service "unifiedpush-server" do
-  down node['unifiedpush']['unifiedpush-server']['ha']
-  options({
-    :log_directory => server_log_dir
-  }.merge(params))
-  log_options node['unifiedpush']['logging'].to_hash.merge(node['unifiedpush']['unifiedpush-server'].to_hash)
+# Add postgres module
+template "#{modules_dir}/module.xml" do
+  owner "root"
+  group node['unifiedpush']['user']['group']
+  mode 0755
+  source "wildfly-postgres-module.xml.erb"
 end
 
-if node['unifiedpush']['bootstrap']['enable']
-  execute "/opt/unifiedpush/bin/unifiedpush-ctl start unifiedpush-server" do
-    retries 20
-  end
+# Copy postgres driver
+remote_file "Copy postgres driver file" do
+  path "#{modules_dir}/postgresql-9.4-1201-jdbc41.jar"
+  source "file://#{install_dir}/embedded/apps/unifiedpush/initdb/lib/postgresql-9.4-1201-jdbc41.jar"
+  owner "root"
+  group node['unifiedpush']['user']['group']
+  mode 0755
 end
-
