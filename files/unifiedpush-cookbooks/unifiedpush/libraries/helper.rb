e68b31ea74e3e774cde74c848de39392e784a0e3
yaniv-cb4 <yanivn@c-b4.com>
2017-02-17 14:54:58 +0200

    Leftover changes to helper.rb

--- files/unifiedpush-cookbooks/unifiedpush/libraries/helper.rb
+++ files/unifiedpush-cookbooks/unifiedpush/libraries/helper.rb
@@ -18,21 +18,24 @@
 require 'mixlib/shellout'
 
 module ShellOutHelper
-
-  def do_shell_out(cmd)
-    o = Mixlib::ShellOut.new(cmd)
+  def do_shell_out(cmd, user = nil, cwd = nil)
+    o = Mixlib::ShellOut.new(cmd, user: user, cwd: cwd)
     o.run_command
     o
+  rescue Errno::EACCES
+    Chef::Log.info("Cannot execute #{cmd}.")
+    o
+  rescue Errno::ENOENT
+    Chef::Log.info("#{cmd} does not exist.")
+    o
   end
-
   def success?(cmd)
     o = do_shell_out(cmd)
     o.exitstatus == 0
   end
-
   def failure?(cmd)
     o = do_shell_out(cmd)
-    o.exitstatus == 3
+    o.exitstatus != 0
   end
 end
 
@@ -105,11 +108,11 @@ class OmnibusHelper
   end
 
   def service_up?(service_name)
-    success?("/opt/unifiedpush/bin/unifiedpush-ctl status #{service_name}")
+    success?("/opt/unifiedpush/embedded/bin/sv status #{service_name}")
   end
 
   def service_down?(service_name)
-    failure?("/opt/unifiedpush/bin/unifiedpush-ctl status #{service_name}")
+    failure?("/opt/unifiedpush/embedded/bin/sv status #{service_name}")
   end
 
   def user_exists?(username)
