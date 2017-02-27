7172f8acb634d5ce09f7f4eeeb62dbc271441025
yaniv-cb4 <yanivn@c-b4.com>
2017-02-27 14:02:14 +0200

    Return postgres user creation to postgres recipe

--- files/unifiedpush-cookbooks/unifiedpush/recipes/postgresql.rb
+++ files/unifiedpush-cookbooks/unifiedpush/recipes/postgresql.rb
@@ -26,6 +26,17 @@ postgresql_user = account_helper.postgresgl_user
 postgresql_group = account_helper.postgresgl_group
 unifiedpush_group = account_helper.unifiedpush_group
 
+account "Postgresql user and group" do
+  username postgresql_user
+  uid node['unifiedpush']['postgresql']['uid']
+  ugid postgresql_user
+  groupname postgresql_group
+  gid node['unifiedpush']['postgresql']['gid']
+  shell node['unifiedpush']['postgresql']['shell']
+  home node['unifiedpush']['postgresql']['home']
+  manage node['unifiedpush']['manage-accounts']['enable']
+end
+
 # Add postgresql user to unifiedpudh group
 group unifiedpush_group do
   action :modify
