1b347bdb5900befa0c555c4552f74c412a88d1e2
yaniv-cb4 <yanivn@c-b4.com>
2017-03-13 15:33:18 +0200

    Start and wait to cassandra local node if enabled

--- files/unifiedpush-cookbooks/unifiedpush/recipes/cassandra.rb
+++ files/unifiedpush-cookbooks/unifiedpush/recipes/cassandra.rb
@@ -44,7 +44,6 @@ end
 
 # Install apache cassandra from external package
 include_recipe 'cassandra-dse' if node['unifiedpush']['cassandra']['enable']
-include_recipe 'unifiedpush::cassandra_keyspace' if node['unifiedpush']['cassandra']['enable']
 
 # Make sure cassandra execution in not bloked by selinux
 # This is required only because installation_dir is symbolic link.
@@ -53,9 +52,48 @@ execute "restorecon-cassandra-slink" do
   action :run
 end
 
-if node['unifiedpush']['bootstrap']['enable']
+# NOTE: These recipes are written idempotently, but require a running
+# Cassandra service.  They should run each time (on the appropriate
+# backend machine, of course), because they also handle schema
+# upgrades for new releases of AeroBase.  As a result, we can't
+# just do a check against node['unifiedpush']['bootstrap']['enable'],
+# which would only run them one time.
+if node['unifiedpush']['cassandra']['enable']
   execute "/opt/unifiedpush/bin/unifiedpush-ctl start cassandra" do
-    retries 5
+    retries 20
   end
-end
 
+  ruby_block "wait for cassandra to start" do
+    block do
+      connectable = false
+      6.times do |i|
+        # Note that we have to include the port even for a local pipe, because the port number
+        # is included in the pipe default.
+        #`nodetool statusgossip`
+        # if $?.exitstatus != 0
+        mode = `nodetool netstats`
+        if !mode.include? "NORMAL"
+          Chef::Log.fatal("Could not connect to local cassandra node, retrying in 10 seconds.")
+          sleep 10
+        else
+          if i > 1
+            Chef::Log.fatal("Cassandra netstats return NORMAL status, waiting additional 10 seconds.")
+            sleep 10
+          end
+          connectable = true
+          break
+        end
+      end
+
+      unless connectable
+        Chef::Log.fatal <<-ERR
+Could not connect to the cassandra database.
+Please check /var/log/unifiedpush/cassandra/current for more information.
+ERR
+        exit!(1)
+      end
+    end
+  end
+
+  include_recipe 'unifiedpush::cassandra_keyspace' if node['unifiedpush']['cassandra']['enable']
+end
