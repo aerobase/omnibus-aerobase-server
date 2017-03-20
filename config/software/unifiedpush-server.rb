fc7b611e965c7c504cedcb71c5bfeee667fd7ed8
yaniv-cb4 <yanivn@c-b4.com>
2017-03-20 11:38:56 +0200

    Update unifiedpush-server sversion

--- config/software/unifiedpush-server.rb
+++ config/software/unifiedpush-server.rb
@@ -24,7 +24,7 @@ dependency "postgresql"
 dependency "wildfly"
 
 version "1.2.0-SNAPSHOT" do
-  source md5: "1615225941ef3e0f554b175f42616ad6"
+  source md5: "062b1e682ab7fabd369677268b99314a"
 end
 
 repo_home = if "#{version}".end_with?("SNAPSHOT") then "libs-snapshot-local" else "libs-release-local" end
