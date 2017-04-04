86bede63d8677d3e3306c84aae2a2e78ea258c46
yaniv-cb4 <yanivn@c-b4.com>
2017-04-04 09:33:20 +0300

    Test KC 2.5.5

--- config/software/keycloak-server.rb
+++ config/software/keycloak-server.rb
@@ -15,12 +15,16 @@
 #
 
 name "keycloak-server"
-default_version "2.4.0.Final"
+default_version "2.5.5.Final"
 
 dependency "ruby"
 dependency "bundler"
 dependency "rsync"
 dependency "unifiedpush-server"
+
+version "2.5.5.Final" do
+  source md5: "e580232fb2a1c7c9d4c89b6fef6a029d"
+end
 
 version "2.4.0.Final" do
   source md5: "12d2c1eedd7cdd60c7edb0e38bdc4f5a"
