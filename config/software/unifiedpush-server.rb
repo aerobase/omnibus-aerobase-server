51396f5117602469a95ad3b7119a087ae80a2069
yaniv-cb4 <yanivn@c-b4.com>
2017-08-13 17:40:42 +0300

    Test new APNS + Matrix service

--- config/software/unifiedpush-server.rb
+++ config/software/unifiedpush-server.rb
@@ -24,7 +24,7 @@ dependency "postgresql"
 dependency "wildfly"
 
 version "1.2.0-SNAPSHOT" do
-  source md5: "ec754275290b58b0f4c038cd29f12361"
+  source md5: "acdf4355512cb3fb6af2d5dd0d11295c"
 end
 
 version "1.2.0-RC1" do
