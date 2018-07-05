93c8c22c79f055616a80518d53762a3c08c983e2
Yaniv Marom Nachumi <yaniv@aerobase.io>
2018-07-05 15:37:37 +0000

    Modify aerobase-ctl to windows

--- config/software/aerobase-ctl.rb
+++ config/software/aerobase-ctl.rb
@@ -22,44 +22,17 @@ license :project_license
 source :path => File.expand_path("files/aerobase-ctl-commands", Omnibus::Config.project_root)
 
 build do
-  block do
-    open("#{install_dir}/bin/aerobase-ctl", "w") do |file|
-      file.print <<-EOH
-#!/bin/bash
-#
-# Copyright:: Copyright (c) 2012 C-B4, Inc.
-# License:: Apache License, Version 2.0
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
-
-# Ensure the calling environment (disapproval look Bundler) does not infect our
-# Ruby environment if aerobase-ctl is called from a Ruby script.
-for ruby_env_var in RUBYOPT \\
-                    BUNDLE_BIN_PATH \\
-                    BUNDLE_GEMFILE \\
-                    GEM_PATH \\
-                    GEM_HOME
-do
-  unset $ruby_env_var
-done
-
-#{install_dir}/embedded/bin/omnibus-ctl aerobase #{install_dir}/embedded/service/omnibus-ctl $@
-       EOH
-    end
-  end
-
-  command "chmod 755 #{install_dir}/bin/aerobase-ctl"
+  if windows?
+    erb source: "aerobase-ctl.bat.erb",
+      dest: "#{install_dir}/bin/aerobase-ctl.bat",
+      mode: 0755, 
+	  vars: {install_dir: "#{install_dir}"}
+  else
+    erb source: "aerobase-ctl.erb",
+      dest: "#{install_dir}/bin/aerobase-ctl",
+      mode: 0755, 
+	  vars: {install_dir: "#{install_dir}"}
+  end 
 
   # additional omnibus-ctl commands
   copy "./*.*", "#{install_dir}/embedded/service/omnibus-ctl/"
