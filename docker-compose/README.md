50d3bd89910b19659febb724077dac408daeabfb
autowebapp <yaniv@aerobase.io>
2019-01-27 22:31:56 +0200

    Improve docker, bind external theme dir + disable default theme cache

--- docker-compose/README.md
+++ docker-compose/README.md
@@ -1,4 +1,7 @@
 ## Aerobase Docker usage guide
+Create host directories to share SSL and Themes with docker.
+1. mkdir /etc/aerobase/ssl
+2. mkdir /var/opt/aerobase/unifiedpush-server/themes
 
 ### HTTPS vs HTTP
 In order to run using SSL / HTTPS, place your certificate files under /etc/aerobase/ssl/$(hostname).crt/.key
@@ -12,4 +15,17 @@ export HOST=test.aerobase.org; docker-compose up --build -d
 ### Configure aerobase image according to your host name
 ```
 docker-compose exec -T --privileged --index=1 aerobase /bin/bash -c "/tmp/setup.sh"
+```
+### Update your hosts file according to selected $HOST
+```
+echo $(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' dockercompose_aerobase_1) test.aerobase.org >> /etc/hosts
+```
+### Open chrome and browse http(s)://test.aerobase.org/auth/admin/aerobase/console
+Username: admin
+Password: 123
+
+### Stop and reset container state
+```
+docker-compose kill
+docker-compose rm
 ```
--- docker-compose/setup.sh
+++ docker-compose/setup.sh
@@ -15,46 +15,27 @@ get_full_path() {
 SCRIPT_PATH="$(get_full_path ./)"
 
 main(){
-  # Download selected version from bintray
-  if [ ! -f "/tmp/aerobase-${AEROBASE_VERSION}.el7.x86_64.rpm" ]; then
-    curl -L "https://dl.bintray.com/aerobase/aerobase-rpm/aerobase-${AEROBASE_VERSION}.el7.x86_64.rpm" -o /tmp/aerobase-${AEROBASE_VERSION}.el7.x86_64.rpm
-  fi
-
-  if [ ! -f "/tmp/keycloak-server-${KEYCLOAK_VERSION}.el7.x86_64.rpm" ]; then
-    curl -L "https://dl.bintray.com/aerobase/aerobase-rpm/keycloak-server-${KEYCLOAK_VERSION}.el7.x86_64.rpm" -o /tmp/keycloak-server-${KEYCLOAK_VERSION}.el7.x86_64.rpm
-  fi
-
-  # Install version 
-  rpm -U --force /tmp/aerobase-*.rpm
-  rpm -U --force /tmp/keycloak-*.rpm
-
   # Setup default configuration
   if [ -f "/etc/aerobase/ssl/$(hostname).crt" ]; then
     PROTOCOL="https"
   else
     PROTOCOL="http"
   fi
-    
+  
+  # Setup host  
   sed -i "s|external_url .*|external_url '${PROTOCOL}://$(hostname)'|g" /etc/aerobase/aerobase.rb
+
+  # Disable theme cacheing 
+  sed -i "s|# keycloak_server\['theme_cache'\] = true|keycloak_server\['theme_cache'\] = false|g" /etc/aerobase/aerobase.rb
  
   # Configure Aerobase
   aerobase-ctl stop
   aerobase-ctl reconfigure --accept-license
 }
 
-# Defaults
-AEROBASE_VERSION="2.2.0-1"
-KEYCLOAK_VERSION="2.2.0-1"
-
 while [ -n "$1" ]; do
     v="${1#*=}"
     case "$1" in
-        --aerobase-version=*)
-            export AEROBASE_VERSION="${v}"
-            ;;
-        --keycloak-version=*)
-            export KEYCLOAK_VERSION="${v}"
-	    ;;
         --debug)
             DEBUG=1
             print_debug "Debug is ON..."
