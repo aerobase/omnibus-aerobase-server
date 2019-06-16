793886f16d679620b1783d31e0c4f3583098730f
3nGiNe <37801603+3nGiNe@users.noreply.github.com>
2019-06-16 11:44:19 +0300

    Update aerobase-set-krb.ps1 (#20)
    * Update aerobase-set-krb.ps1
    
    0. example usage corrected
    1. introduced local variables for windows installation folders

--- config/templates/aerobase-ctl/aerobase-set-krb.ps1
+++ config/templates/aerobase-ctl/aerobase-set-krb.ps1
@@ -12,14 +12,14 @@ Prepare Keytab file for kerberos integration and place it under AEROBASE default
 .PARAMETER SetSpn
 Permanently sets Service Principal Names (SPN) for an Active Directory service account. Requires domain administrator rights.
 .PARAMETER SetKrb5
-Permanently sets krb5.ini to C:/. Requires domain administrator rights.
+Permanently sets krb5.ini to Windows installation folder. Requires domain administrator rights.
 .PARAMETER KeytabDir
  Write keytab (ktpass) command output to directory. Defaults to C:/Aerobase/Configuration.
 .PARAMETER Output
 Outputs / Execute setspn/keytab/krb5.ini commands. Valid options are Exec, Log. Defaults to Log.
 .EXAMPLE
 Execute setspn / keytab / krb5.ini commands
-aerobase-set-krb.ps1 -SetKeytab -SpnUser -SetKrb5 -SpnUser "SPN Account Name" -SpnPass "SPN Account Password"  -Output "Exec"
+aerobase-set-krb.ps1 -SetKeytab -SetSpn -SetKrb5 -SpnUser "SPN Account Name" -SpnPass "SPN Account Password"  -Output "Exec"
 .EXAMPLE
 Only print keytab / krb5.ini commands
 aerobase-set-krb.ps1 -SetKeytab -SetKrb5 -SpnUser "SPN Account Name" -SpnPass "SPN Account Password"
@@ -33,13 +33,17 @@ aerobase-set-krb.ps1 -SetKeytab -SpnUser "aerobase" -SpnPass "123456" -Output "E
 
 Param(
     [Switch]$SetKeytab,
-    [Parameter(HelpMessage = "Write keytab (ktpass) command output to selected directory.")][ValidateNotNullOrEmpty()]$KeytabDir = "C:\",
+    [Parameter(HelpMessage = "Write keytab (ktpass) command output to selected directory.")][ValidateNotNullOrEmpty()]$KeytabDir=$env:SYSTEMDRIVE + "\",
     [Parameter(HelpMessage = "Service Principal Names (SPN) account name for an Active Directory")][ValidateNotNullOrEmpty()]$SpnUser = "",
     [Parameter(HelpMessage = "Service Principal Names (SPN) account password for an Active Directory")][ValidateNotNullOrEmpty()]$SpnPass = "",
     [Switch]$SetSpn,
     [Switch]$SetKrb5,
     [Parameter(HelpMessage = "Outputs / Execute setspn/keytab/krb5.ini commands")][String][ValidateSet("Exec", "Log")]$Output = "Log"
 )
+
+$winroot = $env:SYSTEMROOT
+$system32 = $winroot + '\System32'
+
 
 function is_admin {
     $admin = [Security.Principal.WindowsBuiltInRole]::Administrator
@@ -74,7 +78,7 @@ if ($SetSpn) {
         Write-Error "ERROR: You need Administrator rights to run setspn command"
     }
 	
-    $cmd='C:\Windows\System32\setspn.exe -A HTTP/' + (get_fqdn) + ' ' + $SpnUser
+    $cmd=$system32 + '\setspn.exe -A HTTP/' + (get_fqdn) + ' ' + $SpnUser
     Write-Verbose -Verbose ('setspn command will output: ' + $cmd)
 	
     if ($Output -eq "Exec"){
@@ -87,7 +91,7 @@ if ($SetKeytab) {
         Write-Error "ERROR: You need Administrator rights to run keytab command"
     }
 	
-    $cmd='C:\Windows\System32\ktpass -out ' + $KeytabDir + '/aerobase.keytab' + ' -princ HTTP/' + (get_fqdn) + '@' + (get_domain).ToUpper() + ' -mapUser ' + $SpnUser +  ' -mapOp set -pass ' + $SpnPass + ' -kvno 0 -crypto all -pType KRB5_NT_PRINCIPAL'
+    $cmd=$system32 + '\ktpass -out ' + $KeytabDir + '/aerobase.keytab' + ' -princ HTTP/' + (get_fqdn) + '@' + (get_domain).ToUpper() + ' -mapUser ' + $SpnUser +  ' -mapOp set -pass ' + $SpnPass + ' -kvno 0 -crypto all -pType KRB5_NT_PRINCIPAL'
     Write-Verbose -Verbose ('ktpass command will output: ' + $cmd)
 	
     if ($Output -eq "Exec"){
@@ -104,13 +108,11 @@ forwardable = true
 rdns = false
 default_realm = _DOMAINUPPER_
 default_etypes = aes256-cts-hmac-sha1-96
-
 [realms]
 _DOMAINUPPER_ = {
   kdc = _DOMAINDC_
   admin_server = _DOMAINDC_
 }
-
 [domain_realm]
 ._DOMAIN_ = _DOMAINUPPER_
 _DOMAIN_ = _DOMAINUPPER_
@@ -128,8 +130,7 @@ if ($SetKrb5) {
     
     #$cmd='NOT Implemented Yet'
     Write-Verbose -Verbose ('krb5.ini file will output: ' + $krb5ini)
-	
     if ($Output -eq "Exec"){
-        Out-File -FilePath 'C:\Windows\krb5.ini' -InputObject $krb5ini
+       Out-File -FilePath $winroot + '\krb5.ini' -InputObject $krb5ini
     }
 }
