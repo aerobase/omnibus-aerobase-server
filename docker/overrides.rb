106b5dc9fb4c7e8d575a308129043c6850d3ea79
Yaniv Marom Nachumi <yaniv@aerobase.io>
2022-03-09 10:19:32 +0200

    Add compose example for pgsl

--- docker/overrides.rb
+++ docker/overrides.rb
@@ -2,8 +2,8 @@ external_url 'http://aerobase.example.com'
 keycloak_server['theme_cache'] = false
 
 #postgresql['enable'] = false
-#postgresql['server'] = "172.18.0.1"
-#postgresql['username'] = "postgres"
+#postgresql['server'] = "postgres"
+#postgresql['username'] = "aerobase-sql"
 #postgresql['password'] = "postgres"
 
 aerobase_server['java_xmx'] = "2g"
