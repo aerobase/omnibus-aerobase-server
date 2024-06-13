a3e14551e5a5556d8916d1a851e176e61a6867ac
yaniv <yaniv@aerobase.io>
2024-06-13 09:59:02 +0000

    Fix windows build

--- config/software/aerobase-fix-parser.rb
+++ config/software/aerobase-fix-parser.rb
@@ -8,16 +8,16 @@ license :project_license
 build do
   env = with_standard_compiler_flags(with_embedded_path)
 
-  gem "uninstall parser" \
-      " --version '3.3.2.0'" \
-      " --bindir '#{install_dir}/embedded/bin'"
-  
-  gem "uninstall rubocop-ast" \
-      " --version '1.31.3'" \
-      " --bindir '#{install_dir}/embedded/bin'"
-    
   gem "install parser" \
       " --version '3.3.0.5'" \
       " --bindir '#{install_dir}/embedded/bin'" \
       "  --no-document", env: env
+  
+  gem "uninstall -I rubocop-ast" \
+      " --version '1.31.3'" \
+      " --bindir '#{install_dir}/embedded/bin'", env: env
+
+  gem "uninstall -I parser" \
+      " --version '3.3.2.0'" \
+      " --bindir '#{install_dir}/embedded/bin'", env: env
 end
